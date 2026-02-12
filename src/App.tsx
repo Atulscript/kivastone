d9da02d47f0e7053c33e7fff61fd89bbbee02095
kivastone <kiva@kivastone.com>
2026-02-12 15:49:18 +0530

    Update scroll line design to match live site

--- src/App.tsx
+++ src/App.tsx
@@ -46,83 +46,84 @@ function App() {
 
   return (
     <div className="x-hidden">
-      <NavBar />
-      <img className="header-shadow" src="sha.png" alt="Shadow" />
+      <div className="header-wrapper">
+        <NavBar />
+        <img className="header-shadow" src="sha.png" alt="Shadow" />
 
-      <div className="scroll-text">SCROLL</div>
-      <div className="first-line scroll-line"></div>
+        <div className="scroll-text">SCROLL</div>
+        <div className="scroll-line-white"></div>
 
-      <div className="video-container video-desktop">
-        <iframe
-          src="https://www.youtube.com/embed/o3m0qvFAfd4?playlist=o3m0qvFAfd4&modestbranding=1&autohide=1&autoplay=1&controls=0&showinfo=0&mute=1&loop=1&rel=0"
-          allowFullScreen
-          title="Desktop Background Video"
-        ></iframe>
-      </div>
-      <div className="video-container video-mobile">
-        <iframe
-          src="https://www.youtube.com/embed/ULR3rxkdxyY?playlist=ULR3rxkdxyY&modestbranding=1&autohide=1&autoplay=1&controls=0&showinfo=0&mute=1&loop=1&rel=0"
-          allowFullScreen
-          title="Mobile Background Video"
-        ></iframe>
-      </div>
-
-      <div className="second-line scroll-line"></div>
-      <span className="dot dot-one-top"></span>
-
-      <div className="carousel-wrapper" data-aos="fade-up" data-aos-delay="100" data-aos-duration="800" data-aos-once="true">
-        <Slider {...settings}>
-          <TypeTile tileName="Quartz" imageUrl="https://i.ibb.co/4K4jf1W/f.png" url="levante" />
-          <TypeTile tileName="Porcelain" imageUrl="https://i.ibb.co/TMQYXRD/b.png" url="imperia" />
-          <TypeTile tileName="Semi Precious" imageUrl="https://i.ibb.co/DtWN22R/a.png" url="inventory/semi-precious" />
-          <TypeTile tileName="Granites & Dolomites" imageUrl="https://i.ibb.co/pQvWM5Y/c.png" url="inventory/granite-dolomites" />
-          <TypeTile tileName="Quartzite" imageUrl="quartzite.png" url="inventory/quartzite" />
-          <TypeTile tileName="Marble" imageUrl="https://i.ibb.co/dsvh9jn/e.png" url="inventory/marble" />
-        </Slider>
-      </div>
-
-      <div className="third-line scroll-line"></div>
-      <div className="inspiration mt-4">
-        <InspirationSection isHome={true} mainText="Space" firstText="Find your" />
-      </div>
-
-      <span className="dot dot-two-top"></span>
-      <div className="fourth-line scroll-line"></div>
-
-      <div className="container-fluid">
-        <div className="collection-container mt-5 mb-5">
-          <div className="collection-title">
-            <div className="featured" data-aos="fade-up" data-aos-delay="0" data-aos-duration="800" data-aos-once="true">FEATURED</div>
-            <div className="collection" data-aos="fade-up" data-aos-delay="0" data-aos-duration="800" data-aos-once="true">COLLECTION</div>
-          </div>
-
-          <div className="collection-list desktop">
-            {featuredListFirstRow.map((tile, i) => (
-              <Tile key={`r1-${i}`} tileCode={tile.item_code} category_code={tile.category_code} tileName={tile.title} imageUrl={tile.slab_images} />
-            ))}
-          </div>
-          <div className="collection-list desktop">
-            {featuredListSecondRow.map((tile, i) => (
-              <Tile key={`r2-${i}`} tileCode={tile.item_code} category_code={tile.category_code} tileName={tile.title} imageUrl={tile.slab_images} />
-            ))}
-          </div>
-
-          <div className="collection-list mobile">
-            <Tile tileCode="#ABC1275" tileName="Blue Deep" imageUrl="bluedeep.png" category_code="QUARTZITE" />
-            <Tile tileCode="#34567" tileName="Venatino" imageUrl="venatino.png" category_code="QUARTZ" />
-            <Tile tileCode="#34569" tileName="Calacatta Michelangelo" imageUrl="Michelangelo.png" category_code="PORCELAIN" className="uneven" />
-          </div>
+        <div className="video-container video-desktop">
+          <iframe
+            src="https://www.youtube.com/embed/o3m0qvFAfd4?playlist=o3m0qvFAfd4&modestbranding=1&autohide=1&autoplay=1&controls=0&showinfo=0&mute=1&loop=1&rel=0"
+            allowFullScreen
+            title="Desktop Background Video"
+          ></iframe>
+        </div>
+        <div className="video-container video-mobile">
+          <iframe
+            src="https://www.youtube.com/embed/ULR3rxkdxyY?playlist=ULR3rxkdxyY&modestbranding=1&autohide=1&autoplay=1&controls=0&showinfo=0&mute=1&loop=1&rel=0"
+            allowFullScreen
+            title="Mobile Background Video"
+          ></iframe>
         </div>
       </div>
 
-      <OurCollection collectionText="Our signature quartz collection" imageUrl1="l-logo.png" imageUrl2="pexel.jpg" url="levante" />
-      <OurCollection collectionText="Our signature collection" imageUrl1="i-logo.png" imageUrl2="imperia-1.png" url="imperia" />
+      <div className="content-wrapper">
+        <div className="scroll-line-gray"></div>
+        <span className="dot dot-one-top"></span>
 
-      <span className="dot dot-three-top"></span>
+        <div className="carousel-wrapper" data-aos="fade-up" data-aos-delay="100" data-aos-duration="800" data-aos-once="true">
+          <Slider {...settings}>
+            <TypeTile tileName="Quartz" imageUrl="https://i.ibb.co/4K4jf1W/f.png" url="levante" />
+            <TypeTile tileName="Porcelain" imageUrl="https://i.ibb.co/TMQYXRD/b.png" url="imperia" />
+            <TypeTile tileName="Semi Precious" imageUrl="https://i.ibb.co/DtWN22R/a.png" url="inventory/semi-precious" />
+            <TypeTile tileName="Granites & Dolomites" imageUrl="https://i.ibb.co/pQvWM5Y/c.png" url="inventory/granite-dolomites" />
+            <TypeTile tileName="Quartzite" imageUrl="quartzite.png" url="inventory/quartzite" />
+            <TypeTile tileName="Marble" imageUrl="https://i.ibb.co/dsvh9jn/e.png" url="inventory/marble" />
+          </Slider>
+        </div>
 
-      <div className="visualizatin-container">
-        <div className="fifth-line scroll-line"></div>
-        <VisualizeStone />
+        <div className="inspiration mt-4">
+          <InspirationSection isHome={true} mainText="Space" firstText="Find your" />
+        </div>
+
+        <span className="dot dot-two-top"></span>
+
+        <div className="container-fluid">
+          <div className="collection-container mt-5 mb-5">
+            <div className="collection-title">
+              <div className="featured" data-aos="fade-up" data-aos-delay="0" data-aos-duration="800" data-aos-once="true">FEATURED</div>
+              <div className="collection" data-aos="fade-up" data-aos-delay="0" data-aos-duration="800" data-aos-once="true">COLLECTION</div>
+            </div>
+
+            <div className="collection-list desktop">
+              {featuredListFirstRow.map((tile, i) => (
+                <Tile key={`r1-${i}`} tileCode={tile.item_code} category_code={tile.category_code} tileName={tile.title} imageUrl={tile.slab_images} />
+              ))}
+            </div>
+            <div className="collection-list desktop">
+              {featuredListSecondRow.map((tile, i) => (
+                <Tile key={`r2-${i}`} tileCode={tile.item_code} category_code={tile.category_code} tileName={tile.title} imageUrl={tile.slab_images} />
+              ))}
+            </div>
+
+            <div className="collection-list mobile">
+              <Tile tileCode="#ABC1275" tileName="Blue Deep" imageUrl="bluedeep.png" category_code="QUARTZITE" />
+              <Tile tileCode="#34567" tileName="Venatino" imageUrl="venatino.png" category_code="QUARTZ" />
+              <Tile tileCode="#34569" tileName="Calacatta Michelangelo" imageUrl="Michelangelo.png" category_code="PORCELAIN" className="uneven" />
+            </div>
+          </div>
+        </div>
+
+        <OurCollection collectionText="Our signature quartz collection" imageUrl1="l-logo.png" imageUrl2="pexel.jpg" url="levante" />
+        <OurCollection collectionText="Our signature collection" imageUrl1="i-logo.png" imageUrl2="imperia-1.png" url="imperia" />
+
+        <span className="dot dot-three-top"></span>
+
+        <div className="visualizatin-container">
+          <VisualizeStone />
+        </div>
       </div>
 
       <div data-aos="fade-up" data-aos-offset="200" data-aos-duration="1000" data-aos-once="true">
